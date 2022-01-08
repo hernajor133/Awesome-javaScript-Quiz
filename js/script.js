21bb11eda5078d8e113cbb356b7252fe361500de
Jordi <damlabulbul26@gmail.com>
2022-01-07 20:56:39 -0800

    variables have been declared

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+var startButton = document.querySelector("#start-btn");
+var nextButton = document.querySelector("#next-btn");
+var timerCount = document.querySelector(".timer-count");
+var questionContainerE1 = document.querySelector("#question-container");
+var questionE1 = document.querySelector("#question");
+var answerButtonsE1 = document.querySelector("#answer-btns");
+var instructions = document.querySelector(".instructions-text");
+var form = document.querySelector("#form");
+var scoreText = document.querySelector("#enter-score-text");
+
